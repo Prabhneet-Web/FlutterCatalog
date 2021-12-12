af1de87f89e35c1f2234de2ac0a6ce5a2e935e86
Prabhneet Narula <imailcode@gmail.com>
2021-12-12 14:23:05 +0530

    day1 changes done

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const int days = 30;
+    const String name = "Prabhneet";
+    const double pi = 3.14;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("Welcome to the $days days of Flutter by $name"),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
