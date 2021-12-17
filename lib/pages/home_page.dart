57d1e00dade153f9c27b7d3fb737b09941b6bbbc
Prabhneet Narula <imailcode@gmail.com>
2021-12-17 10:42:27 +0530

    Catalog.json file added.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,9 +3,14 @@ import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
