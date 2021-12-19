3fc543e7369e1b609472648fc70dfe44f75c7b60
Prabhneet Narula <imailcode@gmail.com>
2021-12-20 01:29:31 +0530

    day9 changes done.

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -14,14 +15,57 @@ class homeDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          buttonPadding: EdgeInsets.zero,
+          alignment: MainAxisAlignment.spaceBetween,
+          children: [
+            "₹ ${catalog.price}".text.bold.xl.red900.make(),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    MyTheme.darkBluishColor,
+                  ),
+                  shape: MaterialStateProperty.all(const StadiumBorder())),
+              onPressed: () {},
+              child: "Buy".text.medium.heightTight.make(),
+            ).wh(80, 40),
+          ],
+        ).p32(),
+      ),
       body: SafeArea(
+        bottom: false,
         child: Column(
           children: [
             Hero(
-                tag: Key(catalog.id.toString()),
-                child: Image.network(catalog.image)),
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.image),
+            ).h32(context).p20(),
+            const Divider(),
+            Expanded(
+                child: VxArc(
+              height: 30,
+              arcType: VxArcType.CONVEY,
+              edge: VxEdge.TOP,
+              child: Container(
+                width: context.screenWidth,
+                child: Column(
+                  children: [
+                    catalog.name.text.xl4.bold
+                        .color(MyTheme.darkBluishColor)
+                        .make(),
+                    catalog.desc.text
+                        .textStyle(context.captionStyle!)
+                        .lg
+                        .make(),
+                    10.heightBox,
+                  ],
+                ).py20(),
+              ),
+            ))
           ],
-        ).p16(),
+        ),
       ),
     );
   }
