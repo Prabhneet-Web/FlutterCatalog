003e36c63b6bd03ccc36396c0b058b6a3e75c4ff
Prabhneet Narula <imailcode@gmail.com>
2021-12-23 22:59:22 +0530

    offlineImages added.

--- lib/pages/home_widgets/catalog_image.dart
+++ lib/pages/home_widgets/catalog_image.dart
@@ -7,8 +7,8 @@ class CatalogImage extends StatelessWidget {
   const CatalogImage({Key? key, required this.image}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Image.network(image)
-        .box
+    return Image.asset(image)
+        .box  
         .color(context.canvasColor)
         .make()
         .wh24(context);
